ed03b95f00e0b83420f208bed1d9b3e057acf431
GiuseppeVartuli <giuseppevartuli89@gmail.com>
2023-12-08 16:28:38 +0100

    Feat gioco dei dadi

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,10 @@
+// Mail
+
 /*
 Mail
 Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 
-Gioco dei dadi
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
-
 */
-
-console.log("ciao");
 
 const mail = ["giuseppe@ciao.it", "pippo@baudo.com", "papera@boolean.com"];
 
@@ -18,4 +15,30 @@ if (mail.includes(user)) {
   console.log(user);
 } else {
   alert("Tu non puoi passare!");
+}
+
+// Gioco dei dadi
+
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
+
+const player = Math.floor(Math.random(1) * 7);
+
+Math.floor(Math.random(1) * 7);
+
+console.log(user);
+
+const bot = Math.floor(Math.random(1) * 7);
+
+Math.floor(Math.random(1) * 7);
+
+console.log(bot);
+
+if (player > bot) {
+  alert("Hai battuto il computer!");
+} else if (bot > player) {
+  alert("Il computer ti ha battuto!");
+} else {
+  alert("Avete pareggiato!");
 }
